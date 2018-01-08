a50acbcfb5a149ee3f56fa3e335a5516c7bc0639
mallik <mallik@192.168.0.31>
2018-01-08 17:28:28 -0600

    .

---
+++ src/TestWrappers.java
@@ -0,0 +1,19 @@
+public class TestWrappers {
+	public static void main(String[] args) {
+
+		int i = 10;
+		
+		Integer i1 = new Integer(10);
+	
+		int i3 =i1;
+		
+//		//Auto boxing
+//		Integer i2 = 10;
+//		
+//		System.out.println(i2.hashCode());
+//		
+//		System.out.println(i1 == i2);
+//	
+	}
+
+}
